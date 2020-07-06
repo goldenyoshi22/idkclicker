76c22e05423c71b82b0746adcf3ca43387d329d7
goldenyoshi22 <59319965+goldenyoshi22@users.noreply.github.com>
2020-07-06 14:06:17 -0500

    v1.1
    added prestige

--- script.js
+++ script.js
@@ -1,40 +1,64 @@
 
-function $(id) {return document.getElementById(id)};
+function $(id) {return document.getElementById(id)}; //totally not copy pasted
 
 let D = x => {
   return new OmegaNum(x);
-}
+} //totally not copy pasted 2
 
 var ON = OmegaNum;
 
 var game = {
 greenpoints: D(0),
-greenclickpower: D(1),
+greenpower: D(1),
 redmoney: D(0),
 redpoints: D(0),
-green2unlock: false
+prestiged: false
 }
+
+function notation(num, dec = 2) {
+  if (num.gte(1e6)) {
+    let exponent = num.logBase(10).floor()
+    let mantissa = toFixed(num.div(OmegaNum.pow(10, exponent)), dec)
+    
+    return mantissa + "e" + exponent;
+  }
+  return OmegaNum.round(num.times(OmegaNum.pow(10, dec))).div(
+    OmegaNum.pow(10, dec)) //totally not copy pasted 3
+}
+
 
 function increment(n = 1) {
 	if (n == 1) { 
-	game.greenpoints = game.greenpoints.add(game.greenclickpower); 
+	game.greenpoints = game.greenpoints.add(game.greenpower.mul(game.redpoints.add(2).logBase(2)).round());
 	} else if (n == 2) {		
-	game.greenclickpower = game.greenclickpower.add(1)
-	}
+	game.greenpower = game.greenpower.add(1)
+	} else if (n == 3) {		
+	if (game.redmoney.gte(1)) {
+    game.redmoney = game.redmoney.sub(1)
+	game.redpoints = game.redpoints.add(1)
+	}}
 	updateThings()
 }
 
 function calcPrestige(num) {
-return num.root(1.69).logBase(4).floor()	
+return num.root(1.5).logBase(3.5).floor()	
 }
 
+function prestige() {
+game.redmoney = game.redmoney.add(calcPrestige(game.greenpoints))
+game.greenpoints = D(0)
+game.greenpower = D(1)
+game.prestiged = true
+updateThings()
+}
 function updateThings() {
-	$("number").innerHTML = "you have " + game.greenpoints + " idk points and " + game.greenclickpower + " click power"
-	if (game.greenpoints.gte(69)) $("redprestige").style.display = "inline"; 
+	$("number").innerHTML = "you have " + notation(game.greenpoints) + " green points and " + notation(game.greenpower.mul(game.redpoints.add(2).logBase(2))) + " green power"
+	$("number2").innerHTML = "you have " + notation(game.redmoney) + " red money and " + notation(game.redpoints) + " red points (x" + notation(game.redpoints.add(2).logBase(2)) + " mult)"
+	if (game.greenpoints.gte(25)) $("redprestige").style.display = "inline"; 
 	else $("redprestige").style.display = "none";
-	if (game.green2unlock == true) $("green2").style.display = "inline"; 
-	else $("green2").style.display = "none";
-	$("redprestige").innerHTML = "prestige for " + calcPrestige(game.greenpoints) + " red currency"
+if (game.prestiged == true) {$("green2").style.display = "inline"; $("redprestigearea").style.display = "inline";} 
+	else {$("green2").style.display = "none"; $("redprestigearea").style.display = "none"; }
+	$("redprestige").innerHTML = "prestige for " + notation(calcPrestige(game.greenpoints)) + " red currency"
 }
 
 updateThings()
