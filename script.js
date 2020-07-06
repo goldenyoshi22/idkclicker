4e8a65961ec803f7bc0359f3374731eae5adf708
goldenyoshi22 <59319965+goldenyoshi22@users.noreply.github.com>
2020-07-06 10:48:14 -0500

    Summary (not required)

--- script.js
+++ script.js
@@ -8,10 +8,31 @@ let D = x => {
 var ON = OmegaNum;
 
 var game = {
-idkpoints: D(0)
+greenpoints: D(0),
+greenclickpower: D(1),
+redmoney: D(0),
+redpoints: D(0),
+
 }
 
-function increment() {
-	game.idkpoints = game.idkpoints.add(1)
-    $("number").innerHTML = "you have " + game.idkpoints + " idk points"	
+function increment(n = 1) {
+	if (n == 1) { 
+	game.greenpoints = game.greenpoints.add(game.greenclickpower); 
+	} else if (n == 2) {		
+	game.greenclickpower = game.greenclickpower.add(1)
+	}
+	updateThings()
 }
+
+function calcPrestige(num) {
+return num.root(1.69).logBase(4).floor()	
+}
+
+function updateThings() {
+	$("number").innerHTML = "you have " + game.greenpoints + " idk points and " + game.greenclickpower + " click power"
+	if (game.greenpoints.gte(69)) $("redprestige").style.display = "inline"
+	else $("redprestige").style.display = "none"
+	$("redprestige").innerHTML = "prestige for " + calcPrestige(game.greenpoints) + " red currency"
+}
+
+checkThings()
